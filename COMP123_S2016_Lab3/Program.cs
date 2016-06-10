1b25c96f5883820372a4897cbb79ffddfd13c6ac
gapillan <gpaillan@my.centennialcollege.ca>
2016-06-09 19:56:38 -0700

    Added an Deck instance on the main method

--- COMP123_S2016_Lab3/Program.cs
+++ COMP123_S2016_Lab3/Program.cs
@@ -10,17 +10,17 @@ namespace COMP123_S2016_Lab3
     {
         static void Main(string[] args)
         {
-            Deck deck = new Deck(); // create a deck of cards 
-
-            deck.Display(); // display the initial state of the deck 
-
-            deck.Shuffle(); // shuffle the deck 
-            deck.Display(); // display the deck after shuffling 
-
-            Card cardDelt = deck.Deal();
-            Console.WriteLine("Card Delt: {0} of {1}", cardDelt.Face, cardDelt.Suit);
-
-            deck.Display();
+            Deck deck = new Deck(); // create a deck of cards  
+             
+            deck.Display(); // display the initial state of the deck  
+             
+            deck.Shuffle(); // shuffle the deck   
+            deck.Display(); // display the deck after shuffling  
+             
+            Card cardDelt = deck.Deal(); 
+            Console.WriteLine("Card Delt: {0} of {1}", cardDelt.Face, cardDelt.Suit); 
+             
+            deck.Display(); 
         }
     }
 }
