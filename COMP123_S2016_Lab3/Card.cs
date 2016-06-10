c2d1025b714d57deb6a5ee19b37552a1a673d063
gapillan <gpaillan@my.centennialcollege.ca>
2016-06-09 19:55:25 -0700

    Added a _create, _display and _shuffle constructor method

--- COMP123_S2016_Lab3/Card.cs
+++ COMP123_S2016_Lab3/Card.cs
@@ -8,5 +8,27 @@ namespace COMP123_S2016_Lab3
 {
     class Card
     {
+        // PUBLIC PROPERTIES ++++++++++++++++++++++++++++++++++
+        public int Face { get; set; }
+
+        public string Suit { get; set; }
+
+        // CONSTRUCTORS +++++++++++++++++++++++++++++++++++++++
+
+        /**
+         * <summary>
+         * This constructor takes two parameters face and suit
+         * to instantiate a Card object
+         * </summary>
+         * 
+         * @constructor Card 
+         * @param {int} face
+         * @param {string} suit
+         */
+        public Card(int face, string suit)
+        {
+            this.Face = face;
+            this.Suit = suit;
+        }
     }
 }
--- COMP123_S2016_Lab3/Deck.cs
+++ COMP123_S2016_Lab3/Deck.cs
@@ -6,7 +6,124 @@ using System.Threading.Tasks;
 
 namespace COMP123_S2016_Lab3
 {
-    class Deck
+    class Deck : List<Card>
     {
+        //CONSTRUCTORS 
+        /**
+         * <summary>
+         * this is the default constructor for the deck class 
+         * </summary>
+         * 
+         * @constructor Deck 
+         */
+        public Deck()
+        {
+            // create the deck of cards 
+            this._create();
+        }
+        // PRIVATE METHODS ++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        /**
+         * <summary>
+         * This method loads a list of cards with Card objects
+         * </summary>
+         * 
+         * @private
+         * @method _create
+         * @param {List<Card>} deck
+         * @returns {void}
+         */
+        private void _create()
+        {
+            string suit = "";
+
+            for (int suitIndex = 0; suitIndex < 4; suitIndex++)
+            {
+                switch (suitIndex)
+                {
+                    case 0:
+                        suit = "hearts";
+                        break;
+                    case 1:
+                        suit = "clubs";
+                        break;
+                    case 2:
+                        suit = "diamonds";
+                        break;
+                    case 3:
+                        suit = "spades";
+                        break;
+
+                } // end suit switch
+
+                for (int face = 1; face < 14; face++)
+                {
+                    this.Add(new Card(face, suit));
+                } // end for - face 
+            }
+        }
+
+        /**
+         * <summary>
+         * This method displays a list of Card objects to the console 
+         * </summary>
+         * 
+         * @method Display
+         * @param (List<card>) deck 
+         * @returns {void}
+         */
+        public void Display()
+        {
+            Console.WriteLine("====================================");
+            Console.WriteLine("+            Current Deck          +");
+            Console.WriteLine("====================================");
+            foreach (Card card in this)
+            {
+                Console.WriteLine("{0} of {1}", card.Face, card.Suit);
+            }
+            Console.WriteLine("====================================");
+            Console.WriteLine();
+        }
+
+        /**
+         * this mehtod randomly shuffles a List of Card objects 
+         * 
+         * @method shuffle 
+         * @returns {void}
+         */
+        public void Shuffle()
+        {
+            // creates a psuedo-random number sequence and stores it in random 
+            Random random = new Random();
+            // recod the number of cards in the deck list 
+            int cardCount = this.Count;
+
+            Console.WriteLine("Card Count: {0}", cardCount); // displays the total amount of the deck
+
+            // iterate through the list of cards || note: this is also known as an SWAPPING DATA ALGORITHM 
+            for (int currentCard = 0; currentCard < cardCount; currentCard++)
+            {
+                Card tempCard = this[currentCard]; // copy curretnt card to temp location 
+                int randomCard = random.Next(0, cardCount); // get a random index
+                this[currentCard] = this[randomCard]; // swap values from randomCard to currentCard
+                this[randomCard] = tempCard; // copy current card to the random card
+            }
+        }
+
+        /**
+         * <summary> 
+         * This method will remove the 0th item from the deck and reutrn it to the caller
+         * </summary>
+         * 
+         * @method Deal 
+         * @returns {Card}
+         */
+        public Card Deal()
+        {
+            Card returnedCard = this[0]; // copy the 0th card in the stack to the returnedCard 
+
+            this.RemoveAt(0); // remove the 0th Card from deck 
+            return returnedCard;
+        }
     }
 }
